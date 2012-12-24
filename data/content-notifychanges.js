ab8a9927ceeca105893e9415b18780a3a045948d
Mathieu Pillard <m@virgule.net>
2012-12-24 13:58:40 +0100

    Attach pageMod to every document, even existing ones, and start receiving events earlier.
    - This should remove the need for the 'ready' hack on tabs.
    - This should break iframes, but for the moment they cause performance problems

--- data/content-notifychanges.js
+++ data/content-notifychanges.js
@@ -5,6 +5,29 @@ document.addEventListener('scroll', function(e) {
     self.port.emit('scroll');
 });
 
+// listen to DOMContentLoaded event (document)
+document.addEventListener('DOMContentLoaded', function(e) {
+    self.port.emit('DOMContentLoaded');
+
+    // listen to DOM changes once DOM is ready
+    var observer = new MutationObserver(function(mutations) {
+        // Delay sending dom changes to prevent useless and costly redraws
+        if (!observer._timeout) {
+            observer._timeout = setTimeout(function() {
+                self.port.emit('mutation');
+                observer._timeout = null;
+            }, 1000);
+        }
+    });
+    observer._timeout = null;
+    observer.observe(document.body, {
+        subtree: true,
+        attributes: false,
+        childList: true,
+        characterData: true
+    });
+});
+
 // Listen to resize event (window)
 document.defaultView.addEventListener('resize', function(e) {
     self.port.emit('resize');
@@ -14,24 +37,3 @@ document.defaultView.addEventListener('resize', function(e) {
 document.defaultView.addEventListener('load', function(e) {
     self.port.emit('load');
 });
-
-// listen to DOM changes
-var observer = new MutationObserver(function(mutations) {
-    // Delay sending dom changes to prevent useless and costly redraws
-    if (!observer._timeout) {
-        observer._timeout = setTimeout(function() {
-            self.port.emit('mutation');
-            observer._timeout = null;
-        }, 1000);
-    }    
-});
-observer._timeout = null;
-observer.observe(document.body, { 
-    subtree: true,
-    attributes: false,
-    childList: true,
-    characterData: true
-});
-
-// manually emit a "load" once, since load event has probably happened already
-self.port.emit('load');
