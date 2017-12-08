diox/firefox-minimap-scroller

commit 3d3bf7d3113481fdf5a1bf663b45b9d89dabca7c

Update sidebar (and therefore scroller) dimensions on resize

content-scripts/base.js
@@ -3,7 +3,7 @@
     let portToExtension;
     let sidebarWidth;
     let sidebarHeight;
-    let areEventHandlersSet;
+    let initDone;
     let contentRootElement;
 
     // Basic debounce function.
@@ -109,7 +109,11 @@
     let messageReceiver = (message) => {
         if (message.sidebarWidth && message.sidebarHeight) {
             ({sidebarWidth, sidebarHeight} = message);
-            onSidebarReady();
+            if (!initDone) {
+                // The first time we receive this message, we want to set up
+                // event listeners and do an initial capture.
+                onSidebarReady();
+            }
         }
         if (message.scrollDelta) {
             onSidebarScrollDelta(message.scrollDelta);
@@ -140,22 +144,23 @@
         // Initial capture when we switch to the tab or load the extension.
         captureAndSendData();
 
-        if (!areEventHandlersSet) {
-            // Various events to listen to.
-            window.addEventListener('load', captureAndSendData);
-            document.defaultView.addEventListener(
-                'resize', debounce(captureAndSendData, 250));
-            document.addEventListener('scroll', captureAndSendScrollerOnly);
+        // Various events to listen to.
+        window.addEventListener('load', captureAndSendData);
+        document.defaultView.addEventListener(
+            'resize', debounce(captureAndSendData, 250));
+        document.addEventListener('scroll', captureAndSendScrollerOnly);
 
-            let observer = new MutationObserver(
-                debounce(captureAndSendData, 250));
-            observer.observe(document.documentElement, {
-                subtree: true,
-                attributes: false,
-                childList: true,
-                characterData: true
-            });
-        }
+        let observer = new MutationObserver(
+            debounce(captureAndSendData, 250));
+        observer.observe(document.documentElement, {
+            subtree: true,
+            attributes: false,
+            childList: true,
+            characterData: true
+        });
+
+        // Avoid doing this again.
+        initDone = true;
     };
 
     let onConnectionReady = (port) => {

sidebar/sidebar.js
@@ -6,6 +6,19 @@
     let minimapElm;
     let sidebarRootElm;
 
+    // Basic debounce function.
+    const debounce = (fn, time) => {
+        let timeout;
+
+        return function debounced (...args) {
+            /* eslint-disable no-invalid-this */
+            const func = () => fn.apply(this, args);
+
+            clearTimeout(timeout);
+            timeout = setTimeout(func, time);
+        };
+    };
+
     // FIXME: is sidebar is hidden, maybe tell the content script to not send
     // us anything. Not sure if needed, need to test.
 
@@ -46,10 +59,9 @@
         // given tab at at time, so just overwrite portToCurrentTab.
         portToCurrentTab = browser.tabs.connect(tabId);
 
-        messageSender({
-            'sidebarWidth': sidebarRootElm.offsetWidth,
-            'sidebarHeight': sidebarRootElm.offsetHeight,
-        });
+        // Send sidebar dimensions. That will trigger the initial capture for
+        // this tab.
+        sendSidebarDimensions();
 
         // Listen for new captures sent by the content script we're connected
         // to.
@@ -106,7 +118,8 @@
         scrollerElm.style.display = 'block';
         scrollerElm.style.width = dimensions.width + 'px';
         scrollerElm.style.height = dimensions.height + 'px';
-        scrollerElm.style.transform = `translate(${dimensions.left}px,${dimensions.top}px)`;
+        scrollerElm.style.transform =
+            `translate(${dimensions.left}px,${dimensions.top}px)`;
     };
 
     let isScrollerVisible = () => {
@@ -145,6 +158,13 @@
             }
         });
     };
+
+    let sendSidebarDimensions = () => {
+        messageSender({
+            'sidebarWidth': sidebarRootElm.offsetWidth,
+            'sidebarHeight': sidebarRootElm.offsetHeight,
+        });
+    }
 
     document.addEventListener('DOMContentLoaded', () => {
         sidebarRootElm = document.documentElement;
@@ -224,5 +244,10 @@
                 }
             }
         });
+
+        // When we resize the sidebar, we need to give the content-script the
+        // new sidebar dimensions.
+        document.defaultView.addEventListener(
+            'resize', debounce(sendSidebarDimensions, 100));
     });
 }

commit bd07902e1a0b9ddaaf8a677757312fd71ab7b38c

Fix regression introduced in 3d3bf7d3, redraw on tab switching
(Thanks, @twidi)

content-scripts/base.js
@@ -1,10 +1,10 @@
 /* global browser */
 {
+    let contentRootElement;
+    let initDone;
     let portToExtension;
     let sidebarWidth;
     let sidebarHeight;
-    let initDone;
-    let contentRootElement;
 
     // Basic debounce function.
     const debounce = (fn, time) => {
@@ -110,10 +110,14 @@
     const messageReceiver = (message) => {
         if (message.sidebarWidth && message.sidebarHeight) {
             ({sidebarWidth, sidebarHeight} = message);
-            if (!initDone) {
+            if (message.hasOwnProperty('forceDraw')) {
+                captureAndSendData();
+
                 // The first time we receive this message, we want to set up
                 // event listeners and do an initial capture.
-                onSidebarReady();
+                if (!initDone) {
+                    initContentScript();
+                }
             }
         }
         if (message.scrollDelta) {
@@ -141,10 +145,7 @@
         contentRootElement.scrollLeft = (scrollAbsolute.absoluteX / ratio);
     };
 
-    const onSidebarReady = () => {
-        // Initial capture when we switch to the tab or load the extension.
-        captureAndSendData();
-
+    const initContentScript = () => {
         // Various events to listen to.
         window.addEventListener('load', captureAndSendData);
         document.defaultView.addEventListener(

sidebar/sidebar.js
@@ -61,7 +61,7 @@
 
         // Send sidebar dimensions. That will trigger the initial capture for
         // this tab.
-        sendSidebarDimensions();
+        sendSidebarDimensions({forceDraw: true});
 
         // Listen for new captures sent by the content script we're connected
         // to.
@@ -159,10 +159,11 @@
         });
     };
 
-    const sendSidebarDimensions = () => {
+    const sendSidebarDimensions = (extra) => {
         messageSender({
             'sidebarWidth': sidebarRootElm.offsetWidth,
             'sidebarHeight': sidebarRootElm.offsetHeight,
+            ...extra,
         });
     }
 